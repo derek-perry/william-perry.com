8bda38c9ab06a1d64d6765bb18e2a0129a8550c5
DP <dp@derek-perry.com>
2023-11-29 21:09:10 -0500

    Add Calendar and Repertoire Pages
    Created calendar and repertoire pages with initial functionality ready for tests. Data is coming from a Google Sheet.

---
+++ pages/calendar.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import type { GetServerSideProps, NextPage } from 'next';
+import { getEvents } from '../lib/api';
+import SiteHead from '../components/SiteHead';
+import PageFooter from '../components/PageFooter';
+
+interface listingsProps {
+  listings: listingsInnerProps[]
+}
+
+interface listingsInnerProps {
+  name: string,
+  datetime: string,
+  price: string,
+  description: string
+}
+
+const calendarPage: NextPage<listingsProps> = ({ listings }) => {
+  function stringWithLineBreaks(inputString: string) {
+    var outputString = inputString.toString().replace(/\n/g, "<br />");
+    return outputString;
+  }
+  function stringWithUrlSupport(inputString: string) {
+    var outputString = inputString.trim().toString().toLowerCase().replace(/\s+/g, '-').replace(/ - /g, "-").replace(/---/g, "-").replace(/\&/g, "and").replace(/;/g, "%3B").replace(/:/g, "%3A").replace(/"/g, "%22").replace(/'/g, "%27").replace(/,/g, "%2C").replace(/\?/g, "%3F").replace(/!/g, "%21").replace(/@/g, "%40").replace(/#/g, "%23").replace(/\$/g, "%24").replace(/&/g, "%26").replace(/\*/g, "%2A").replace(/=/g, "%3D").replace(/\+/g, "%2B").replace(/\(/g, "%28").replace(/\)/g, "%29").replace(/\[/g, "%5B").replace(/\]/g, "%5D").replace(/\\/g, "%5C").replace(/\//g, "%2F");
+    return outputString;
+  }
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <SiteHead title="Calendar - William Perry" description="William-Perry.com is the official website for William Perry - Pianist, Educator, and Conductor" url="calendar" />
+
+      <main className="bg-white text-wpBlack w-full flex flex-1 flex-col text-center items-center justify-center px-9">
+        <section id="calendar">
+          <section className="max-md:mt-10 md:mt-20 mb-10 max-w-[1000px] text-left">
+            <h1 className="mb-4 text-6xl">Calendar</h1>
+          </section>
+        </section>
+
+        <section id="listings">
+          <div className="mb-28 max-w-[1000px] w-full overflow-hidden flex flex-row flex-wrap gap-x-8 gap-y-8 items-top justify-center text-center text-xl">
+            {listings
+              .slice(1)
+              .map(({ name, datetime, price, description }) => (
+                <article
+                  className="rounded bg-wpWhite overflow-hidden w-full max-w-full xl:max-w-[500px]"
+                  key={name}
+                  id={stringWithUrlSupport(name)}
+                >
+                  <h3 className="bg-wpRedLight text-white min-h-[88px] flex items-center justify-center font-bold text-3xl px-5 mb-3 max-sm:hyphens-auto">{name}</h3>
+                  <p className="mt-4 mb-2 text-2xl px-5">{datetime}</p>
+                  <p className="mb-4 text-2xl px-5">{price}</p>
+                  <p className="mt-2 mb-4 max-sm:hyphens-auto text-left px-5" dangerouslySetInnerHTML={{ __html: stringWithLineBreaks(description) }} />
+                </article>
+              ))}
+          </div>
+        </section>
+      </main>
+
+      <PageFooter />
+    </div>
+  );
+};
+
+export default calendarPage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const listings = await getEvents();
+
+  return {
+    props: {
+      listings
+    }
+  };
+};
---
+++ pages/repertoire.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import type { GetServerSideProps, NextPage } from 'next';
+import { getSongs } from '../lib/api';
+import SiteHead from '../components/SiteHead';
+import PageFooter from '../components/PageFooter';
+
+interface listingsProps {
+  listings: listingsInnerProps[]
+}
+
+interface listingsInnerProps {
+  name: string,
+  artist: string,
+  year: string,
+  genre: string
+}
+
+const repertoirePage: NextPage<listingsProps> = ({ listings }) => {
+  function stringWithUrlSupport(inputString: string) {
+    var outputString = inputString.trim().toString().toLowerCase().replace(/\s+/g, '-').replace(/ - /g, "-").replace(/---/g, "-").replace(/\&/g, "and").replace(/;/g, "%3B").replace(/:/g, "%3A").replace(/"/g, "%22").replace(/'/g, "%27").replace(/,/g, "%2C").replace(/\?/g, "%3F").replace(/!/g, "%21").replace(/@/g, "%40").replace(/#/g, "%23").replace(/\$/g, "%24").replace(/&/g, "%26").replace(/\*/g, "%2A").replace(/=/g, "%3D").replace(/\+/g, "%2B").replace(/\(/g, "%28").replace(/\)/g, "%29").replace(/\[/g, "%5B").replace(/\]/g, "%5D").replace(/\\/g, "%5C").replace(/\//g, "%2F");
+    return outputString;
+  }
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <SiteHead title="Repertoire - William Perry" description="William-Perry.com is the official website for William Perry - Pianist, Educator, and Conductor" url="repertoire" />
+
+      <main className="bg-white text-wpBlack w-full flex flex-1 flex-col text-center items-center justify-center px-9">
+        <section id="repertoire">
+          <section className="max-md:mt-10 md:mt-20 mb-10 max-w-[1000px] text-left">
+            <h1 className="mb-4 text-6xl">Repertoire</h1>
+          </section>
+        </section>
+
+        <section id="listings">
+          <div className="mb-28 max-w-[1000px] w-full overflow-hidden flex flex-col gap-x-8 gap-y-8 items-top justify-center text-center">
+            {listings
+              .slice(1)
+              .map(({ name, artist, year, genre }) => (
+                <article
+                  className="p-5 flex max-md:flex-col md:flex-row text-left odd:bg-white even:bg-wpWhite"
+                  key={name}
+                  id={stringWithUrlSupport(name)}
+                >
+                  <p className="text-2xl md:max-w-[280px] md:min-w-[280px] md:border-r px-4"><strong>{name}</strong></p>
+                  <p className="text-2xl md:max-w-[180px] md:min-w-[180px] md:border-r px-4">{artist}</p>
+                  <p className="text-2xl md:max-w-[85px] md:min-w-[85px] md:border-r px-4">{year}</p>
+                  <p className="text-2xl px-4">{genre}</p>
+                </article>
+              ))}
+          </div>
+        </section>
+      </main>
+
+      <PageFooter />
+    </div>
+  );
+};
+
+export default repertoirePage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const listings = await getSongs();
+
+  return {
+    props: {
+      listings
+    }
+  };
+};
